4d377fc279c96ce596315c983ce2e4aabb5216c6
Isael Olmedo Mayo <ioolmedomay@uncg.edu>
2026-04-02 10:36:57 -0400

    change to in-class-event-listener.html/script2.js

---
+++ script2.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("thisButton");
+// find the button
+button.addEventListener("click", function() {
+// wait for click
+ document.getElementById("message").innerText =
+"Why Are Funko Pops So Expensive";
+ // change the text
+ document.body.style.backgroundColor = "purple";
+ // change the background color
+ document.body.style.fontSize ="50px"
+ // change the text color
+});
+
+
