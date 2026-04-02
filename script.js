f98b479dd1cbe62d9a9fdd1641866a2226ebece1
Isael Olmedo Mayo <ioolmedomay@uncg.edu>
2026-04-02 10:14:45 -0400

    changes to in-class-event.html/scipt.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('thisButton');
+    button.addEventListener('click', function(){
+        document.getElementById('message').innerText ="Honda Civics Are The Best";
+    });
+
+
